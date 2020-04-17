b2a7e00d2dfa6a5b59aa14a9a82a778610cbfaee
benjables <xbenjablesx@gmail.com>
2020-04-16 21:49:07 -0400

    added plugin.yml

--- Reimund/src/Brushes/SmoothBrush.java
+++ Reimund/src/Brushes/SmoothBrush.java
@@ -1,8 +1,20 @@
 package Brushes;
 
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
 public class SmoothBrush {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
+		if (sender instanceof ConsoleCommandSender) return false;
+		Player player = (Player) sender;
+		Block block = player.getTargetBlock(null,200);
+		Location loc = block.getLocation();
+		
 		return true;
 	}
 
