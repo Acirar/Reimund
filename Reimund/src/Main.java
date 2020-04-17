a04af79e1f0a003eaaa10c73517be1bab9e7da69
benjables <xbenjablesx@gmail.com>
2020-04-16 21:27:30 -0400

    added onEnable and onDisable

--- Reimund/src/Main.java
+++ Reimund/src/Main.java
@@ -1,7 +1,17 @@
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 	
+	private static Logger logger = Logger.getLogger("Minecraft");
 	
+	public void onEnable() {
+		logger.info("(Reimund has been enabled)");
+	}
+	
+	public void onDisable() {
+		logger.info("(Reimund has been disabled)");
+	}
 }
