1fa26bb362aca7bbe9134e7ab8b6c757ced28f38
benjables <xbenjablesx@gmail.com>
2020-04-16 21:37:20 -0400

    Update BallBrush.java

--- Reimund/src/Brushes/BallBrush.java
+++ Reimund/src/Brushes/BallBrush.java
@@ -3,19 +3,17 @@ package Brushes;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BallBrush extends JavaPlugin{
 	public boolean onCommand(CommandSender sender, Command cmd, String string, String [] args) {
-		return false;
-		
-		@Command(
-			name = "Sphere",	
-			aliases = "s",	
-			desc = "Creates a sphere"	
-		)
-		@CommandPermissions("Reimund.brush.sphere")
+		if (sender instanceof ConsoleCommandSender) return false;
+		if (cmd.getName().equalsIgnoreCase("sphere")) {
+			Player player = (Player) sender;
+			//alright, now write the rest of your code for the command here (delete this after you read it)
+		}
+		return true;
 	}
-	
-	
 };
