27915a3047296ebdcf01a40c6045700a8a777799
Acirar <63758077+Acirar@users.noreply.github.com>
2020-04-17 11:28:51 +1000

    Updated BallBrush
    Updated Ballbrush cmd

--- Reimund/src/Brushes/BallBrush.java
+++ Reimund/src/Brushes/BallBrush.java
@@ -1,11 +1,21 @@
 package Brushes;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BallBrush extends JavaPlugin{
-	
-	
+	public boolean onCommand(CommandSender sender, Command cmd, String string, String [] args) {
+		return false;
+		
+		@Command(
+			name = "Sphere",	
+			aliases = "s",	
+			desc = "Creates a sphere"	
+		)
+		@CommandPermissions("Reimund.brush.sphere")
+	}
 	
 	
 };
